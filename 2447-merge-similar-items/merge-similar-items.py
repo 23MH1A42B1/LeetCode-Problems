27aa1f9aa2f59378103329ef066a0e576c41df73
MURALI <nadipenamurali13@gmail.com>
2026-02-13 20:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (46.51%) - LeetSync

---
+++ 2447-merge-similar-items/merge-similar-items.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        
+        weight_map = {}
+        for value, weight in items1:
+            weight_map[value] = weight_map.get(value, 0) + weight
+        for value, weight in items2:
+            weight_map[value] = weight_map.get(value, 0) + weight
+        return sorted([[value, weight] for value, weight in weight_map.items()])
