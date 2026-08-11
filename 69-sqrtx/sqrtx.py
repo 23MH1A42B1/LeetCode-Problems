40a8a8d3d70480e78f62b4c35599c81bcfccf9e4
MURALI <nadipenamurali13@gmail.com>
2026-08-11 14:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.89%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+        left,right=0,x//2
+        ans=0
+        while left<=right:
+            mid=(left+right)//2
+            num=mid*mid
+            if num==x:
+                return mid
+            elif num<x:
+                ans=mid
+                left=mid+1
+            else:
+                right=mid-1
+        return ans
